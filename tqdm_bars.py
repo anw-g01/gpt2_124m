0918ae62bea581d4b60ecda57ed98dc64fb86c7c
Anwarat Gurung <agurung_01@outlook.com>
2025-01-07 17:11:34 +0000

    added a custom-made progress bar for GPT-2 training, and tracks several stats as well

--- tqdm_bars.py
+++ tqdm_bars.py
@@ -6,6 +6,56 @@ Custom tqdm progress bars for logging stats with:
 
 from tqdm import tqdm
 
+
+class tqdmGPT(tqdm):
+    """Custom and optional tqdm progress bar for training GPT-2."""
+
+    def __init__(self, *args, acc_steps: int, n_tokens: int, **kwargs):
+        bar_format_str = (
+            "[{bar:15}] {n_fmt}/{total_fmt} {percentage:.1f}% | "
+            "{tok/s} | {s/batch} ({ms/iter}) "
+            "[{elapsed}<{remaining}, {batches/s}] | "
+            "{desc}"
+        )
+        params = {
+            "bar_format": bar_format_str,
+            "ascii": "-#",
+            "mininterval": 1,
+            "colour": "cyan"
+        }
+        for key, value in params.items():
+            kwargs.setdefault(key, value)
+        self.n_tokens = n_tokens                # no. of tokens processed
+        self.steps = acc_steps                  # no. of gradient accumulation steps (to find iterations rate)
+        super().__init__(*args, **kwargs)       # pass to constructor of parent class
+
+    @property
+    def format_dict(self):
+        d = super().format_dict     # get format dictionary from parent tqdm class
+        d["n_fmt"] = f"{d['n']:,}" if d["n"] else "?"                 # current iteration (tokens processed) in billions
+        d["total_fmt"] = f"{d['total']:,}" if d["total"] else "?"     # total iterations (tokens to process) in billions
+        if d["rate"]:               # if rate exists (avoid division by zero)
+            tpb = 1 / d["rate"]     # time per batch
+            if tpb < 1:             # if less than a second
+                d["s/batch"] = f"{tpb * 1e3:.1f} ms/batch"          # milliseconds per step (batch)
+            else:
+                d["s/batch"] = f"{tpb:.2f} s/batch"                 # in milliseconds if faster
+            d["ms/iter"] = f"{tpb / self.steps * 1e3:.1f} ms/iter"  # milliseconds per iter (per accumulation step)
+            d["batches/s"] = f"{d['rate']:,.2f} batch/s"            # batches per second (forward + backward passes)
+        else:
+            d["s/batch"], d["ms/iter"], d["batches/s"] = "? s/batch", "? ms/iter", "? batches/s"
+        # calculate training tokens processed per second
+        if d["elapsed"]:
+            token_rate = (self.steps * self.n_tokens * self.n) / d["elapsed"]       # self.n keeps track of current iteration (from PARENT tqdm class)
+            if token_rate < 1e6:
+                d["tok/s"] = f"{token_rate:,.0f} tok/s"
+            else:
+                d["tok/s"] = f"{token_rate * 1e-6:.2f}M tok/s"
+        else:
+            d["tok/s"] = "? tok/s"
+        return d
+    
+
 class tqdmFW(tqdm):
     """Custom and optional tqdm progress bar for FineWeb-Edu Sample-10BT dataset processing."""
     
