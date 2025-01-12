c4198278a0d0549bbd688416b0529b588abd22f4
Anwarat Gurung <agurung_01@outlook.com>
2025-01-12 01:33:53 +0000

    completed the evaluate function to iterate across all hellaswag examples and simulate a candidate prediction to compare against the ground truth labels; can currently be used to evaluate using pretrained model like gpt2 (124M) or gpt-xl (1.5B)

--- hellaswag.py
+++ hellaswag.py
@@ -6,6 +6,7 @@ import tiktoken
 import requests
 import json
 from tqdm import tqdm
+from tqdm_bars import tqdmHS
 import os
 
 
@@ -18,7 +19,7 @@ DATASETS = {
 ENC = tiktoken.get_encoding("gpt2")     # GPT2 tokenizer for encoding
 
 
-def get_file(url: str, filename: str, chunk_size=1024):
+def _get_file(url: str, filename: str, chunk_size=1024):
     """Download a file from a given `url` and save it to a specified `filename`."""
     with requests.get(url, stream=True) as response:
         total = int(response.headers.get("content-length", 0))
@@ -36,17 +37,26 @@ def get_file(url: str, filename: str, chunk_size=1024):
                 pbar.update(size)                               # update bar by bytes progress
 
 
-def download(split: str):
+def _download(split: str):
     """Download the HellaSwag dataset for a given `split` and save it  to `DATA_CACHE_DIR`."""
     os.makedirs(DATA_CACHE_DIR, exist_ok=True)                              # create cache directory if it doesn't exist already
     url = DATASETS[split]                                                   # get URL for the specified split
     filename = os.path.join(DATA_CACHE_DIR, f"hellaswag_{split}.jsonl")     # create filename for the split
     if not os.path.exists(filename):                                        # if file doesn't exist
         print(f"downloading {url} to {filename}...")
-        get_file(url, filename)                                             # download the file into the directory path
+        _get_file(url, filename)                                             # download the file into the directory path
 
 
-def render_example(example: dict) -> tuple:
+def iterate_examples(split: str):
+    """Iterate over examples in the HellaSwag dataset for a given `split`."""
+    _download(split)                                        
+    filename = os.path.join(DATA_CACHE_DIR, f"hellaswag_{split}.jsonl")     
+    with open(filename, "r") as file:                       
+        for line in file:               # each line is an example
+            yield json.loads(line)      # yield the example as a JSON object (dictionary)
+
+
+def render(example: dict) -> tuple:
     """
     Render a given `example` dictionary into a suitable format for evaluation.
     
@@ -84,5 +94,90 @@ def render_example(example: dict) -> tuple:
 
     return data, tokens, mask, data["label"]
 
+
+@torch.no_grad()
+def evaluate(
+        model_type: str = "gpt2",
+        split: str = "train",
+        compile: bool = False,
+        verbose: bool = False,
+        device: str = "cuda"
+    ) -> tuple:
+    """
+    Evaluate a given `model_type` on the HellaSwag dataset for a given `split`.
+    Loads examples iteratively and calculates the average cross entropy loss of
+    the model's predictions for each set of ending candidates. The candidate with
+    the lowest loss is chosen as the predicted ending. A tally of correct predictions
+    against ground truth labels is kept and returned at the end of evaluation.
+    """
+
+    torch.set_float32_matmul_precision('high') # use tf32
+    model = GPT2LMHeadModel.from_pretrained(model_type).to(device)
+    model = torch.compile(model) if compile else model
+
+    total, correct = 0, 0
+    pbar = tqdmHS(
+        iterable=iterate_examples(split),
+        total=DATASETS[split][1],
+        desc=f"correct: 0/0",
+        disable=not verbose
+    )
+
+    for i, example in enumerate(pbar):
+        data, tokens, mask, label = render(example)
+        T, M = tokens.to(device), mask.to(device)
+
+        P = model(T).logits                     # get all logits from forward pass
+
+        P = P[:, :-1, :].contiguous()           # remove last prediction (nothing to predict after ending)
+        T = T[:, 1:].contiguous()               # remove first token (no previous token to predict it)
+
+        L = F.cross_entropy(
+            input=P.view(-1, P.shape[-1]),      # --> [N, vocab_size] (where N = batch_size * seq_len)
+            target=T.view(-1),                  # --> [N,]
+            reduction="none"                    # retain individual losses
+        )                                       # --> [N,]
+        L = L.view(T.shape[0], -1)              # reshape back to [batch_size * seq_len]
+
+        M = M[:, 1:].contiguous()               # shift mask same as tokens tensor (T)
+        L *= M                                  # apply mask element-wise (zero all context token positions)
+
+        avg_L = L.sum(dim=1) / M.sum(dim=1)     # average along each sample candidate
+        y_pred = avg_L.argmin().item()          # lower loss => more confident
+
+        # accumulate stats
+        total += 1
+        correct += int(y_pred == label)
+
+        if verbose and (i % 100 == 0):          # progress logging if verbose=True
+            correct_pct = correct / (i + 1) * 100
+            progress_str = (
+                f"correct: {correct:,}/{i:,} ({correct_pct:.1f}%)"
+            )
+            pbar.set_description_str(progress_str)
+        
+    return correct, total
+
+
+def main() -> None:
+    """
+    Evaluate on pretrained `GPT-2` (124M) on the HellaSwag training dataset:
+    
+    `[===============] 39,905/39,905 (100.0%) | correct: 12,356/39,900 (31.0%) [06:10<00:00, ? examples/s]`
+
+    Final results score: `correct: 12,358/39,905 (31.0%)`
+
+    Use `model_type="gpt2-xl"` for the `1.5B` parameter `GPT-2` model; default is `"gpt2"`.
+    """
+
+    correct, total = evaluate(verbose=True)     # evaluate GPT-2 (124M) on HellaSwag
+    
+    # display results:
+    score = correct / total * 100
+    print(f"total correct: {correct:,}/{total:,} ({score:.1f}%)")
+
+    
+
 if __name__ == "__main__":
-    pass
+    
+    main()      # evaluate with a pretrained model (with progress bar logging)
