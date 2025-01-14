246892a10113ae321028fd4144488338b44fcfd8
Anwarat Gurung <agurung_01@outlook.com>
2025-01-14 15:45:16 +0000

    removed ITERATIONS and MAX_STEPS due to switching to using EPOCHS in train.py

--- config.py
+++ config.py
@@ -1,22 +1,21 @@
 # ----- GLOBAL VARIABLES ----- #
 
-DATA_ROOT           = r"C:\Users\aguru\Downloads\fineweb-edu-10BT"   # your directory where shards are stored
+DATA_ROOT           = r"C:\Users\aguru\Downloads\fineweb-edu-10BT"   # your directory where FineWeb-Edu shards are stored
 
 TOKENS_PER_BATCH    = 2 ** 19       # 2^19 = 524,288 
 BATCH_SIZE          = 16            # mini-batch size (samples per forward pass)
 BLOCK_SIZE          = 1024          # context (sequence) length
 WEIGHT_DECAY        = 0.1           # applied on specific parameter groups (see model.configure_optim() method)
 
-ITERATIONS          = 19_073        # no. of model updates during training
+EPOCHS              = 1             # no. of cycles over the full dataset
 LOG_INTERVAL        = 1             # print stats every 'interval' steps
-VAL_INTERVAL        = 100           # validation every 'interval' steps
+VAL_INTERVAL        = 250           # validation every 'interval' steps
 VAL_ACCUM_STEPS     = 20            # no. of validation mini-batches to run and average
 
 # learning rate scheduler parameters:
-LEARNING_RATE       = 6e-4              # maximum learning rate
-WARMUP_STEPS        = 715               # steps over linearly increasing LEARNING_RATE
-MAX_STEPS           = ITERATIONS        # steps over which cosine decay will take place
+MAX_LEARNING_RATE   = 6e-4          # maximum learning rate
+WARMUP_STEPS        = 715           # steps over linearly increasing LEARNING_RATE
 
-# for Tiny Shakespeare dataset:
+# for Shakespeare dataset:
 PCT_DATA            = 1             # proportion of full data used (Tiny Shakespeare)
 TRAIN_SPLIT         = 0.9           # 10% reserved for validation 
