7538c43ebda0929a43c7ab7fed4c724da0f4a49d
Anwarat Gurung <agurung_01@outlook.com>
2025-01-15 01:20:41 +0000

    created a detailled tqdmGPT example with a dummy training loop using pbar.write() to use as experimentation

--- tqdm_bars.py
+++ tqdm_bars.py
@@ -5,23 +5,24 @@ Custom tqdm progress bars for logging stats with:
 """
 
 from tqdm import tqdm
+import time    # using time.sleep() for a dummy training loop
+import datetime
 
 
 class tqdmGPT(tqdm):
-    """Custom and optional tqdm progress bar for training GPT-2."""
+    """Custom `tqdm` progress bar for the `GPT-2` training loop."""
 
     def __init__(self, *args, acc_steps: int, n_tokens: int, **kwargs):
         bar_format_str = (
-            "[{bar:15}] {n_fmt}/{total_fmt} {percentage:.1f}% | "
+            "[{bar:15}] {percentage:.1f}% | i: {n_fmt}/{total_fmt} | "
             "{tok/s} | {s/batch} ({ms/iter}) "
-            "[{elapsed}<{remaining}, {batches/s}] | "
-            "{desc}"
+            "[{elapsed}<{remaining}, {batches/s}] | {desc}"
         )
         params = {
             "bar_format": bar_format_str,
-            "ascii": "-#",
-            "mininterval": 1,
-            "colour": "cyan"
+            "ascii": "-█",
+            "miniters": 100,                    # minimum number iterations between updates
+            "colour": "red"
         }
         for key, value in params.items():
             kwargs.setdefault(key, value)
@@ -44,13 +45,13 @@ class tqdmGPT(tqdm):
             d["batches/s"] = f"{d['rate']:,.2f} batch/s"            # batches per second (forward + backward passes)
         else:
             d["s/batch"], d["ms/iter"], d["batches/s"] = "? s/batch", "? ms/iter", "? batches/s"
-        # calculate training tokens processed per second
+        # calculate training tokens processed per second:
         if d["elapsed"]:
             token_rate = (self.steps * self.n_tokens * self.n) / d["elapsed"]       # self.n keeps track of current iteration (from PARENT tqdm class)
             if token_rate < 1e6:
                 d["tok/s"] = f"{token_rate:,.0f} tok/s"
             else:
-                d["tok/s"] = f"{token_rate * 1e-6:.2f}M tok/s"
+                d["tok/s"] = f"{token_rate * 1e-6:,.2f}M tok/s"
         else:
             d["tok/s"] = "? tok/s"
         return d
@@ -58,8 +59,8 @@ class tqdmGPT(tqdm):
 
 class tqdmHS(tqdm):
     """
-    Custom and optional tqdm progress bar for evaluating on HellaSwag.
-    Only displayed if `verbose=True` in `evaluate()`, from `hellaswag.py`.
+    Custom tqdm progress bar for evaluating on a HellaSwag dataset.
+    Only displayed if `verbose=True` in `evaluate()` - see `hellaswag.py`.
     """
 
     def __init__(self, *args, **kwargs):
@@ -88,7 +89,7 @@ class tqdmHS(tqdm):
     
 
 class tqdmFW(tqdm):
-    """Custom and optional tqdm progress bar for FineWeb-Edu Sample-10BT dataset processing."""
+    """Custom tqdm progress bar for FineWeb-Edu Sample-10BT dataset processing."""
     
     def __init__(self, *args, **kwargs):
         params = {
@@ -110,3 +111,41 @@ class tqdmFW(tqdm):
         else:
             d["rate_fmt"] = f"{d['rate'] * 1e-6:.2f}M tok/sec" if d["rate"] else "?"    # in millions
         return d
+    
+
+if __name__ == "__main__":
+
+    # Example usage of tqdmGPT progress bar with a dummy training loop:
+
+    n_batches, n_epochs = 100, 3
+    n_iterations = n_batches * n_epochs
+    print(f"\ntraining for {n_epochs} epochs, with {n_batches:,} iterations per epoch...")
+    print(f"total iterations in dummy loop: {n_batches:,}\n")
+
+    pbar = tqdmGPT(
+            iterable=range(n_iterations),
+            n_tokens=241_561,
+            acc_steps=4,
+            miniters=1,
+    )
+    for i in pbar:
+        epoch = i // n_batches    # current epoch
+        local_i = i % n_batches   # current step index within current epoch
+        train_loss, val_loss = 2-i/340, 3-i/270   # random dummy values
+        hs_score = 34 + i/246
+        if local_i == n_batches - 1:
+            pct = (i + 1) / n_iterations * 100
+            t = datetime.timedelta(seconds=int(pbar.format_dict["elapsed"]))    # total elapsed time
+            pbar.refresh()
+            pbar.write(
+                f"\nepoch {epoch + 1}/{n_epochs} complete | i: {i + 1:,}/{n_iterations:,} ({pct:.1f}%) | "
+                f"train_loss: {train_loss:.3f} | val_loss: {val_loss:.3f} | "
+                f"HellaSwag: {hs_score:.1f}% | elapsed: [{t}]\n"
+            )
+        if i % 1000 == 0:
+            pbar.set_description_str(
+                f"t_loss: {train_loss:.3f} | "   # dummy values to test
+                f"v_loss: {val_loss:.3f} "  
+            )
+        time.sleep(0.01)    # simulate a pause
+    pbar.close()
