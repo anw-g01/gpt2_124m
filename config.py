99b277e5c8ca85c051d9719edef411df2c1a27ed
Anwarat Gurung <agurung_01@outlook.com>
2025-01-15 21:05:46 +0000

    removed LOG_INTERVAL as printing will currently occur every validation step

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ BLOCK_SIZE          = 1024          # context (sequence) length
 WEIGHT_DECAY        = 0.1           # applied on specific parameter groups (see model.configure_optim() method)
 
 EPOCHS              = 1             # no. of cycles over the full dataset
-LOG_INTERVAL        = 1             # print stats every 'interval' steps
 VAL_INTERVAL        = 250           # validation every 'interval' steps
 VAL_ACCUM_STEPS     = 3             # no. of validation mini-batches to run per GPU
 
