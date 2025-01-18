84211c58e769a90a4715ed19959b69634d849ee9
Anwarat Gurung <agurung_01@outlook.com>
2025-01-18 22:31:20 +0000

    added display_graphs for final setup; compile=False by default for training

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from train import train_gpt2
+from train import train_gpt2, display_graphs
 
 
 def main() -> None:
-    model = train_gpt2(compile=False)           
+    model = train_gpt2()           
 
 
 if __name__ == "__main__":
+
     main()
+
+    # select a checkpoint file to display after training:
+    display_graphs(
+        ddp_world_size=8,
+        num_epochs=1,
+        iter_idx=18850
+    )
