e0cff3215bee1c5fba614da3c56aba514a9148e8
Anwarat Gurung <agurung_01@outlook.com>
2025-01-28 13:29:24 +0000

    added a separate load_model function to load model weights from a checkpoint and sample from the model; added extra iter_num value check when truncating arrays for plotting

--- results.py
+++ results.py
@@ -11,7 +11,7 @@ from model import GPT2_124M, GPT2Config
 
 
 def display_graphs(
-        ddp_world_size: int,
+        used_gpus: int,
         epoch_num: int,
         iter_num: int,
         truncate: bool = True,
@@ -30,32 +30,39 @@ def display_graphs(
     it can also plot the learning rate on the same subplot as the losses. The figure can
     be saved as an image file in the specified `format` type.
 
-    Parameters:
+    Args:
     --
+        `used_gpus` (`int`): number of GPUs used for training.
         `epoch_num` (`int`): epoch number (starting from `1`) for the checkpoint.
         `iter_num` (`int`): iteration number (starting from `1`) for the checkpoint.
         `log_dir` (`str`): directory where the logs and checkpoints are stored.
-        `checkpoint_type` (`str`): prefix of the checkpoint file name (must be `"end"` or `"val"`).
+        `checkpoint_type` (`str`): prefix of the checkpoint file name (must be `"end"` or `"val"`).  Default: `"end"`.
         `plot_lr` (`bool`): whether to plot the learning rate on the same plot as the losses.
         `save` (`bool`): whether to save the plot as an image file.
-        `format` (`str`): format to save the plot image (e.g., `"png"`, `"jpg"`).   
+        `img_format` (`str`): format to save the plot image (e.g., `"png"`, `"jpg"`).   
     """
     assert checkpoint_type in ["end", "val"], "checkpoint_type must be 'end' or 'val'"
+
     # get the checkpoint directory from the filename (for the specified epoch and iteration):
-    filename = _get_checkpoint_filename(checkpoint_type, epoch_num, iter_num, ddp_world_size)
+    filename = _get_checkpoint_filename(checkpoint_type, epoch_num, iter_num, used_gpus)
     checkpoint_dir = os.path.join(log_dir, filename)
     assert os.path.exists(checkpoint_dir), f"checkpoint directory does not exist: {checkpoint_dir}"     # check if the directory exists
+    print(f"checkpoint file: {filename}")
+    print(f"\nloading training plots...\n")
     # load numpy arrays from the checkpoint directory:
     train_losses = np.load(os.path.join(checkpoint_dir, "train_losses.npy"))
     val_losses = np.load(os.path.join(checkpoint_dir, "val_losses.npy"))
     hellaswag_scores = np.load(os.path.join(checkpoint_dir, "hellaswag_scores.npy"))
-    learning_rates = np.load(os.path.join(checkpoint_dir, "learning_rates.npy"))        
+    learning_rates = np.load(os.path.join(checkpoint_dir, "learning_rates.npy"))      
+      
     # truncate arrays up to iter_idx (default):
     if truncate:
-        train_losses = train_losses[:iter_num]
-        val_losses = val_losses[:iter_num]
-        hellaswag_scores = hellaswag_scores[:iter_num]
-        learning_rates = learning_rates[:iter_num]
+        idx = min(iter_num - 1, len(train_losses))    # in case iter_idx > len(train_losses) 
+        train_losses = train_losses[:idx]
+        val_losses = val_losses[:idx]
+        hellaswag_scores = hellaswag_scores[:idx]
+        learning_rates = learning_rates[:idx]
+
     # ----- MAIN FIGURE ----- #
     fig, axs = plt.subplots(nrows=1, ncols=2)
     fig.suptitle(f"GPT2 Training Results: {filename}")  # title for the entire figure
@@ -77,7 +84,7 @@ def display_graphs(
         label="val_loss", color="tab:purple"
     )
     axs[0].axhline(y=3.292, color="tab:red", linestyle="--", label="OpenAI GPT-2 (124M) baseline")
-    axs[1].xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'{int(x):,}'))    # comma separator for x-axis tickers
+    axs[0].xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'{int(x):,}'))    # comma separator for x-axis tickers
     # optional: plot learning rates in a twin axis (with losses figure):
     if plot_lr:
         lr_color = "tab:cyan"    
@@ -116,10 +123,11 @@ def display_graphs(
             dpi=300, bbox_inches="tight",
             format=img_format
         )
-    plt.show()
+    if not save:
+        plt.show()      # code will pause until the plot window is closed
 
     # get directory path to the .pt checkpoint file:
-    print(f"\nloading model and state_dict from checkpoint: {filename}")
+    print(f"\nloading model weights...\n")
     model_checkpoint_dir = os.path.join(checkpoint_dir, "model_checkpoint.pt")      # path to the .pt file holding dictionary of checkpoints file
     checkpoint = torch.load(model_checkpoint_dir)                                   # load the .pt file for dictionary of checkpoints
     # create a new model instance and load the state_dict from the dictionary:
@@ -129,11 +137,69 @@ def display_graphs(
     model.sample("Hello, I'm a")     
 
 
+def load_model(
+        used_gpus: int,
+        epoch_num: int,
+        iter_num: int,
+        log_dir: str = LOG_DIR,
+        checkpoint_type: str = "end",
+    ) -> GPT2_124M:
+    """
+    Load a trained model from a specified checkpoint directory.
+
+    Args:
+    --
+        `used_gpus` (`int`): number of GPUs used for training.
+        `epoch_num` (`int`): epoch number (starting from `1`) for the checkpoint.
+        `iter_num` (`int`): iteration number (starting from `1`) for the checkpoint.
+        `log_dir` (`str`): directory where the logs and checkpoints are stored.
+        `checkpoint_type` (`str`): prefix of the checkpoint file name (must be `"end"` or `"val"`). Default: `"end"`.
+
+    Returns:
+    --
+        `model` (`GPT2_124M`): GPT-2 model instance with loaded weights from a checkpoint.
+    """
+    # get the checkpoint directory from the filename (for the specified epoch and iteration):
+    filename = _get_checkpoint_filename(checkpoint_type, epoch_num, iter_num, used_gpus)
+    checkpoint_dir = os.path.join(log_dir, filename)
+    assert os.path.exists(checkpoint_dir), f"checkpoint directory does not exist: {checkpoint_dir}"     # check if the directory exists
+    print(f"loading checkpoint: {filename}...")
+    
+    # get full path to the .pt file holding dictionary of checkpoints:
+    full_path = os.path.join(checkpoint_dir, "model_checkpoint.pt")  
+    checkpoint = torch.load(full_path)      # load the .pt file for dictionary of checkpoints
+
+    # create a new model instance and load the state_dict from the dictionary:
+    print(f"loading model weights...\n")
+    model = GPT2_124M(GPT2Config(vocab_size=50304))         # create new model instance, must be same config as trained model
+    model.load_state_dict(checkpoint["model_state_dict"])   # load the model state_dict from the checkpoint dictionary
+
+    return model    # return the trained model
+
 if __name__ == "__main__":
+
+    # --- DISPLAY TRAINING CURVES --- #
 
     # select a checkpoint file to display after training:
     display_graphs(
-        ddp_world_size=8,
-        num_epochs=1,
-        iter_idx=18850
+        used_gpus=8,
+        epoch_num=1,
+        iter_num=18850
+    )
+
+    # --- LOAD MODEL + GENERATE SAMPLES --- #
+
+    # separately load a trained model from a checkpoint:
+    model = load_model(
+        used_gpus=8,
+        epoch_num=1,
+        iter_num=18850
+    )
+
+    # generate text samples from the model:
+    model.sample(
+        text="From the depths of",      # starting input text to feed the model
+        n_seqs=3,                       # number of (sequences) samples to generate
+        max_length=150,                 # maximum length of each generated sequence
+        top_k=1000                      # higher k-value for more diverse samples
     )
