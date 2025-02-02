b863395dba79b288a09a82f63efe71e0ad48bdce
Anwarat Gurung <agurung_01@outlook.com>
2025-02-02 20:38:50 +0000

    test run configs

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 # ----- GLOBAL VARIABLES ----- #
 
-DATA_ROOT           = r"C:\Users\aguru\Downloads\fineweb-edu-sample-10BT"   # directory where FineWeb-Edu shards are stored
+DATA_ROOT           = r"fineweb-edu-sample-10BT"   # directory where FineWeb-Edu shards are stored
 LOG_DIR             = r"gpt2_training_checkpoints"                          # directory to write model checkpoints during training
 
 TOKENS_PER_BATCH    = 2 ** 19       # 2^19 = 524,288 
-BATCH_SIZE          = 16            # mini-batch size (samples per forward pass)
+BATCH_SIZE          = 64            # mini-batch size (samples per forward pass)
 BLOCK_SIZE          = 1024          # context (sequence) length
 WEIGHT_DECAY        = 0.1           # applied on specific parameter groups (see model.configure_optim() method)
 
-EPOCHS              = 1             # no. of cycles over the full dataset
-VAL_INTERVAL        = 250           # validation every 'interval' steps
-VAL_ACCUM_STEPS     = 3             # no. of validation mini-batches to run per GPU
-CHECKPOINT_INTERVAL = 10            # write model checkpoints every 'interval' validations
+EPOCHS              = 0.0055        # no. of cycles over the full dataset
+VAL_INTERVAL        = 10            # validation every 'interval' steps
+VAL_ACCUM_STEPS     = 10            # no. of validation mini-batches to run per GPU
+CHECKPOINT_INTERVAL = 2             # write model checkpoints every 'interval' validations
 
 # learning rate scheduler parameters:
-MAX_LEARNING_RATE   = 6e-4          # maximum learning rate
-WARMUP_STEPS        = 715           # steps over linearly increasing LEARNING_RATE
+MAX_LEARNING_RATE   = 2e-3          # maximum learning rate
+WARMUP_STEPS        = 20            # steps over linearly increasing LEARNING_RATE
 
 # for Shakespeare dataset:
 PCT_DATA            = 1             # proportion of full data used (Tiny Shakespeare)
