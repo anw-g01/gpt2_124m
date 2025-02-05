e8d77108803cf9aee0a88fb67cb96fec3014e8ca
Anwarat Gurung <agurung_01@outlook.com>
2025-02-05 20:41:51 +0000

    compiling model (+ eval HellaSwag by default) during training due to no clear issues

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from train import train_gpt2
 
 
 def main() -> None:
-    train_gpt2()           
+    train_gpt2(
+        compile=True,
+        # eval=False        # uncomment: don't evaulate on HellaSwag IF issues arise with torch.compile()
+    )           
 
 
 if __name__ == "__main__":
